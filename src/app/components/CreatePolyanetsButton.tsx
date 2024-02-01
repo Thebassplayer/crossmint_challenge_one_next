81bafbf01a559c5e785e04246964f3b115877cca
Roy Lopez <roylopezdev@gmail.com>
2024-02-01 19:46:18 -0300

    Add CreatePolyanetsButton component

---
+++ src/app/components/CreatePolyanetsButton.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useMegaverseAPI } from "@/hooks/useMegaverseAPI";
+import React, { useState } from "react";
+
+const CreatePolyanetsComponent = () => {
+  const { loading, error, data, performBatchAction, postData } =
+    useMegaverseAPI();
+  const [isCreating, setIsCreating] = useState(false);
+
+  const createPolyanet = async () => {
+    setIsCreating(true);
+
+    const createPolyanetRequests = {
+      row: "1",
+      column: "1", // Adjust the column as needed
+    };
+    console.log(createPolyanetRequests);
+
+    await postData(createPolyanetRequests);
+
+    setIsCreating(false);
+  };
+
+  const createPolyanets = async () => {
+    setIsCreating(true);
+
+    const createPolyanetRequests = Array.from({ length: 10 }, (_, index) => ({
+      row: (index + 1).toString(),
+      column: "3", // Adjust the column as needed
+    }));
+    console.log(createPolyanetRequests);
+
+    await performBatchAction({ polyanets: createPolyanetRequests });
+
+    setIsCreating(false);
+  };
+
+  return (
+    <>
+      <div className="border border-white h-10 w-10">
+        <button onClick={createPolyanet} disabled={isCreating}>
+          {isCreating ? "Creating..." : "Generate Polyanet"}
+        </button>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        {data && <p>Created Polyanets: {JSON.stringify(data)}</p>}
+      </div>
+    </>
+  );
+};
+
+export default CreatePolyanetsComponent;
