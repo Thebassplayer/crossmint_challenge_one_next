72380732622c801ac5d6900e945f21f0de041c02
Roy Lopez <roylopezdev@gmail.com>
2024-02-01 15:59:51 -0300

    Refactor PolyanetRequest function and import processPolyanetRequest

--- src/app/api/polyanets/batch/route.ts
+++ src/app/api/polyanets/batch/route.ts
@@ -1,32 +1,6 @@
-const API_URL = process.env.CROSSMINT_API_BASE_URL;
+import { processPolyanetRequest } from "../route";
+
 const CANDIDATE_ID = process.env.CANDIDATE_ID;
-
-type PolyanetRequest = {
-  method: string;
-  headers: Record<string, string>;
-  body: string;
-};
-
-export async function processPolyanetRequest(requestData: any, method: string) {
-  try {
-    const request: PolyanetRequest = {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestData),
-    };
-
-    console.log(request);
-
-    const response = await fetch(`${API_URL}/polyanets`, request);
-    const data = await response.json();
-    return { data, status: response.status };
-  } catch (error) {
-    console.error(error);
-    return { error, status: 500 };
-  }
-}
 
 export async function POST(req: Request) {
   try {
